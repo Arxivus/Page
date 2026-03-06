9df0ecaeff70ff11dfc0ce9a9545f7289c1e380b
Arxivus <deduykhin_evgenii@mail.ru>
2026-03-06 23:06:01 +0500

    first commit

---
+++ animations.js
@@ -0,0 +1,77 @@
+document.addEventListener('DOMContentLoaded', () => {
+    showHiddenSections()
+
+    setTimeout(() => {
+        const statBlocks = document.querySelectorAll('.about-us-stat__block');
+
+        function animateBlock(index) {
+            if (index >= statBlocks.length) return;
+
+            const statBlock = statBlocks[index];
+            const numberEl = statBlock.querySelector('.about-us-stat__block__stat-number');
+            const endValue = Number(numberEl.getAttribute('value'));
+
+            statBlock.classList.add(`about-us-number-animation${index + 1}`);
+
+            const result = animateNumbers(numberEl, 0, endValue, 2)
+            if (result) {
+                setTimeout(() => animateBlock(index + 1), 300)
+            };
+
+        }
+
+        animateBlock(0)
+    }, 1000);
+
+    const blocks = document.querySelectorAll('.menu-section__block');
+    blocks.forEach(block => {
+        const img = block.querySelector('.menu-section__block__hover-image');
+
+        block.addEventListener('mousemove', (e) => {
+            const rect = block.getBoundingClientRect();
+            const x = e.clientX - rect.left;
+            const y = e.clientY - rect.top;
+
+            img.style.left = x - img.offsetWidth / 2 + 'px';
+            img.style.top = y - img.offsetHeight / 2 + 'px';
+        });
+    });
+
+});
+
+
+function showHiddenSections() {
+    const hiddenElements = document.querySelectorAll('.hidden');
+
+    function checkElementsVisibility() {
+        const triggerBottom = window.innerHeight * 0.9
+
+        hiddenElements.forEach(elem => {
+            const positionOnTop = elem.getBoundingClientRect().top;
+            if (positionOnTop < triggerBottom) {
+                elem.classList.add('visible');
+            }
+        })
+    }
+
+    window.addEventListener('scroll', checkElementsVisibility);
+    checkElementsVisibility()
+}
+
+function animateNumbers(numberEl, startValue, endValue, duration) {
+    if (startValue >= endValue) return false;
+
+    let current = startValue
+    const increment = 1
+    const stepTime = Math.abs(Math.floor(duration * 10))
+
+    const timer = setInterval(function () {
+        current += increment
+        numberEl.innerHTML = current
+        if (current == endValue) {
+            clearInterval(timer)
+        }
+    }, stepTime);
+
+    return true
+}
---
+++ slider.js
@@ -0,0 +1,33 @@
+let currentSlideIndex = 1;
+let slides = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    slides = document.querySelectorAll(".review-section__slider__slide")
+    showSlides(currentSlideIndex, slides);
+})
+
+
+function nextSlide() {
+    showSlides(currentSlideIndex += 1, slides);
+}
+
+function previousSlide() {
+    showSlides(currentSlideIndex -= 1, slides);  
+}
+
+function showSlides(index, slides) {
+
+    if (window.innerWidth < 768) { return } 
+
+    else {
+        if (index > slides.length) { currentSlideIndex = 1 }
+        if (index < 1) { currentSlideIndex = slides.length }
+
+        for (let slide of slides) {
+            slide.style.display = "none";
+        }
+
+        slides[currentSlideIndex - 1].style.display = "flex"
+        slides[index - 1].style.animation = 'fade .6s ease';   
+    } 
+}
